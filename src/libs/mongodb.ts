8c092266dd18b9a1c27fe3d64d16ff6ee593120d
Valentyn <xdshkaa2@gmail.com>
2023-06-01 14:32:12 +0300

    master: added cluster link

--- src/libs/mongodb.ts
+++ src/libs/mongodb.ts
@@ -3,12 +3,13 @@ import mongoose from 'mongoose';
 import config from '../config';
 
 const getCommonConnectLink = () => `mongodb://${config.mongodb.host}:${config.mongodb.port}/${config.mongodb.database}`;
+const getClusterConnectLink = () => `mongodb+srv://${config.mongodb.username}:${config.mongodb.password}@${config.mongodb.host}/${config.mongodb.database}?retryWrites=true&w=majority`;
 
 export default function () {
   mongoose.set('strictQuery', false);
 
   return mongoose.connect(
-    getCommonConnectLink(),
+    getClusterConnectLink(),
     config.mongodb.options,
   );
 }
