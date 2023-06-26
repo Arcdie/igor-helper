232b0870f23d75b18e690230a5b541e08f41c0f4
Valentyn <xdshkaa2@gmail.com>
2023-06-26 13:15:04 +0300

    master: filled public files

--- frontend/public/javascripts/web/publicFiles.js
+++ frontend/public/javascripts/web/publicFiles.js
@@ -5,11 +5,33 @@ vars,
 */
 
 /* Constants */
+const fileList = {
+  auxhp: [
+
+  ],
+
+  thermav: [
+    'Інструкуція_з_монтажу_Hydrosplit_R_32_Ukrainian.pdf',
+    'Інструкція користувача.pdf',
+    'Каталог LG_Therma V 2021.pdf',
+    'ТhermaV Installation manual UKR.pdf',
+    'ТhermaV Service and installation manual UKR.pdf',
+    'KR_ThermaV_Hydrosplit(R32_50Hz)_HP_EU_Gen1.pdf',
+    'KR_ThermaV_SplitR32_50Hz_ 9 kW.pdf',
+    'KR_ThermaV_SplitR410A_50Hz_ IDU.pdf',
+    'KR_ThermaV_SplitR410A_50Hz_ ODU.pdf',
+    'LG_Therma-V_Leaflet_2022.pdf',
+    'Therma V owner ukraine.pdf',
+    'ThermaV_Comprehensive_application_and_installation_manual_ENG.pdf',
+  ],
+};
 
 /* JQuery */
+const $fileList = $('.ih-files');
 
 $(document).ready(async () => {
   disableLink();
+  fillFileList();
 });
 
 const disableLink = () => {
@@ -22,4 +44,26 @@ const disableLink = () => {
       $e.addClass('disabled');
     }
   });
+};
+
+const fillFileList = () => {
+  // const siteName = 'thermav';
+  const siteName = location.host.split('.')[0];
+  const files = fileList[siteName] || [];
+
+  let appendStr = '';
+
+  files.forEach(file => {
+    appendStr += `<div class="ih-file-container col-12 col-xl-4">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${file}</h5>
+          <p class="card-text"></p>
+          <a class="btn btn-secondary float-end" href="/files/${file}" download="${file}">Завантажити</a>
+        </div>
+      </div>
+    </div>`;
+  });
+
+  $fileList.html(appendStr);
 };
